bfa6ac4ab35cb32696dd2af97f7a1b57f013a31b
Ziba Parsons <zibapars@umich.edu>
2023-02-06 18:05:53 -0500

    adding the plot functionality to the package
    Different types of plots for finetuning Kapa and Beta parameters and performance comparison of different methods (test accuracy rate and loss values) are provided in this python file and utils/plot_utils.py file.

---
+++ main_plot.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import importlib
+import random
+import os
+from utils.plot_utils import *
+import torch
+torch.manual_seed(1)
+
+if(1): # plot for MNIST convex
+    numusers = 3
+    num_glob_iters = 50
+    dataset = "Mnist"
+    local_ep = [5,5,20,20]
+    lamda = [30,30,15,15]
+    learning_rate = [0.01, 0.01, 0.005, 0.005]
+    beta =  [0.001, 0.001, 0.001, 1.0]
+    batch_size = [20,20,20,20]
+    K = [5,5,5,5,5,5]
+    personal_learning_rate = [0.01,0.01,0.1,0.1]
+    algorithms = ["RWSADMM","RWSADMM_p"]
+    plot_summary_one_figure_mnist_Compare(num_users=numusers, loc_ep1=local_ep, Numb_Glob_Iters=num_glob_iters, lamb=lamda,
+                                          beta=learning_rate, beta = beta, algorithms_list=algorithms, batch_size=batch_size, dataset=dataset, k = K, personal_learning_rate = personal_learning_rate)
+
+if(0): # plot for Synthetic covex
+    numusers = 20
+    num_glob_iters = 800
+    dataset = "Synthetic" #"Cifar10"
+    local_ep = [20,20,20,20]
+    lamda = [15,15,15,15]#[20,20,20,20]
+    learning_rate = [0.01, 0.01,0.01,0.01]#[0.005, 0.005, 0.005, 0.005]
+    beta =  [0.01, 0.01,1.0,1.0]#[1.0, 1.0, 0.001, 1.0]
+    batch_size = [20,20,20,20]
+    K = [5,5,5,5]
+    personal_learning_rate = [0.01,0.01,0.01,0.01]
+
+    algorithms = ["RWSADMM","pFedMe_p"] #[FedMe_p","RWSADMM","FedAvg","PerAvg_p"]
+    plot_summary_one_figure_synthetic_Compare(num_users=numusers, loc_ep1=local_ep, Numb_Glob_Iters=num_glob_iters, lamb=lamda,
+                               learning_rate=learning_rate, beta = beta, algorithms_list=algorithms, batch_size=batch_size, dataset=dataset, k = K, personal_learning_rate = personal_learning_rate)
+    # plot_summary_one_figure_mnist_Beta(num_users=numusers, loc_ep1=local_ep, Numb_Glob_Iters=num_glob_iters, lamb=lamda,
+    #                            learning_rate=learning_rate, beta = beta, algorithms_list=algorithms, batch_size=batch_size, dataset=dataset, k=K, personal_learning_rate = personal_learning_rate)
